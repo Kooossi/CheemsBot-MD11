02d9892d05b464a1c38905ef3bbfb31588074b00
Kooossi <168139095+Kooossi@users.noreply.github.com>
2024-07-24 04:04:08 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,61 @@
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//owmner v card
+global.ytname = "YT: Trills" //ur yt chanel name
+global.socialm = "Insta: lil_trills" //ur github or insta name
+global.location = "Nigeria, Lagos, Badagry" //ur location
+
+//new
+global.botname = 'Trills Bot MD V11' //ur bot name
+global.ownernumber = '2347025369036' //ur owner number
+global.ownername = '🦄드림 가이 Trills' //ur owner name
+global.websitex = "https://youtu.be/-Omye-W0pTE"
+global.wagc = "https://wa.me/qr/O2VNE2KEHPJZB1"
+global.themeemoji = '🪀'
+global.wm = "Trills Bot Inc."
+global.botscript = 'https://github.com/DGXeon/CheemsBot-MD10' //script link
+global.packname = "Sticker By"
+global.author = "🦄드림 가이 Xeon\n\n+2347025369036"
+global.creator = "2347025369036@s.whatsapp.net"
+global.xprefix = '.'
+global.premium = ["2347025369036"] // Premium User
+global.hituet = 0
+
+//bot sett
+global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typereply = 'v2' // reply type 'v1' => 'v3'
+global.autoblocknumber = '92' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.welcome = false //welcome/left in groups
+global.anticall = false //bot blocks user when called
+global.autoswview = false //auto status/story view
+global.adminevent = false //show promote/demote message
+global.groupevent = false //show update messages in group chat
+//msg
+global.mess = {
+	limit: 'Your limit is up!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+    done: 'Done✓',
+    error: 'Error!',
+    success: 'Here you go!'
+}
+//thumbnail
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
